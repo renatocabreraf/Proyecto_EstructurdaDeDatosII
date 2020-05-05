e639dbacb7a1ef635c47984a71be8998e2a4d58f
Renato <renatojose98@yahoo.com>
2020-05-04 20:33:06 -0600

    Data
    Listas para pruebas temporales

---
+++ Proyecto_EDII/Proyecto_EDII/Data.cs
@@ -0,0 +1,27 @@
+﻿using Proyecto_EDII.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_EDII
+{
+	public class Data
+	{
+		private static Data instance = null;
+		public static Data Instance
+		{
+			get
+			{
+				if (instance == null) instance = new Data();
+				return instance;
+			}
+		}
+
+		public SDES cipherMethods = new SDES();
+		//listas para pruebas temporales 
+		public List<Sucursal> sucursales = new List<Sucursal>();
+		public List<Producto> productos = new List<Producto>();
+		public List<Sucursal_Producto> sucursales_productos = new List<Sucursal_Producto>();
+	}
+}
